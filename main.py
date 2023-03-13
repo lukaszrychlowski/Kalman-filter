7d97c7e07fe4ca56ce5baee81bc3f01c5560e347
lukaszrychlowski <rychlowski@agh.edu.pl>
2023-03-13 14:29:44 +0100

    predict, tests

--- main.py
+++ main.py
@@ -2,9 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class KalmanFilter:
-    def __init__(self, init_x: float, init_v: float) -> None:
-        self.x = np.array([init_x, init_v])  #state mean grv
-        self.P = np.eye(2)                  #covariance of state grv
+    def __init__(self, init_x: float, init_v: float, acc_variance: float) -> None:
+        self._x = np.array([init_x, init_v])  #state mean grv
+        self._acc_variance = acc_variance
+        self._P = np.eye(2)                   #covariance of state grv
 
-kf = KalmanFilter(init_x=0.2, init_v=0.1)
+    def predict(self, dt: float) -> None:
+        #x = F * x
+        #P = F P Ft + G Gt a
+        F = np.array([[1, dt], [0,1]])
+        new_x = F.dot(self._x)
+        G = np.array([0.5 * dt**2, dt]).reshape((2,1))
+        new_P = F.dot(self._P).dot(F.T) + G.dot(G.T) * self._acc_variance
+
+        self._P = new_P
+        self._x = new_x
+
+    @property
+    def position(self) -> float:
+        return self._x[0]
+    
+    @property
+    def velocity(self) -> float:
+        return self._x[1]
+
+    @property
+    def cov(self) -> np.array:
+        return self._P
+    
+    @property
+    def  mean(self) -> np.array:
+        return self._x
+    
+
+kf = KalmanFilter(init_x=0.2, init_v=0.1, acc_variance=0.2)
 
--- test_kalman.py
+++ test_kalman.py
@@ -5,7 +5,23 @@ class TestKalmanFilter(unittest.TestCase):
     def test_can_construct(self):
         x = 0.2
         v = 0.1
+        kf = KalmanFilter(init_x=x, init_v=v, acc_variance=0.2)
+        self.assertAlmostEqual(kf.position,x)
+        self.assertAlmostEqual(kf.velocity,v)
 
-        kf = KalmanFilter(init_x=x, init_v=v)
-        self.assertAlmostEqual(kf.x[0],x)
-        self.assertAlmostEqual(kf.x[1],v)
+    def test_can_predict(self):
+        x = 0.2
+        v = 0.1
+        acc_var = 0.1
+        kf = KalmanFilter(init_x=x, init_v=v, acc_variance=acc_var)
+        kf.predict(dt=1)    
+
+    def test_mean_cov_shapes(self):
+        x = 0.2
+        v = 0.1
+        acc_var = 0.1
+        kf = KalmanFilter(init_x=x, init_v=v, acc_variance=acc_var)
+        kf.predict(dt=1)   
+        
+        self.assertEqual(kf.cov.shape, (2,2))
+        self.assertEqual(kf.mean.shape, (2,))
