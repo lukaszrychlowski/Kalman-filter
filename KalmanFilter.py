e70ab3940397d753fdad321112ee7b61e2637822
lukaszrychlowski <rychlowski@agh.edu.pl>
2023-03-16 00:22:45 +0100

    plots

---
+++ KalmanFilter.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class KalmanFilter:
+    def __init__(self, init_x: float, init_v: float, acc_variance: float) -> None:
+        self._x = np.array([init_x, init_v])  #state mean grv
+        self._acc_variance = acc_variance
+        self._P = np.eye(2)                   #covariance of state grv
+
+    def predict(self, dt: float) -> None:
+        #x = F * x
+        #P = F P Ft + G Gt a
+        F = np.array([[1, dt], [0,1]])
+        new_x = F.dot(self._x)
+        G = np.array([0.5 * dt**2, dt]).reshape((2,1))
+        new_P = F.dot(self._P).dot(F.T) + G.dot(G.T) * self._acc_variance
+
+        self._P = new_P
+        self._x = new_x
+
+    @property
+    def position(self) -> float:
+        return self._x[0]
+    
+    @property
+    def velocity(self) -> float:
+        return self._x[1]
+
+    @property
+    def cov(self) -> np.array:
+        return self._P
+    
+    @property
+    def  mean(self) -> np.array:
+        return self._x
+    
--- test_kalman.py
+++ test_kalman.py
@@ -1,5 +1,6 @@
-from main import KalmanFilter
+from KalmanFilter import KalmanFilter
 import unittest 
+import numpy as np
 
 class TestKalmanFilter(unittest.TestCase):
     def test_can_construct(self):
@@ -22,6 +23,21 @@ class TestKalmanFilter(unittest.TestCase):
         acc_var = 0.1
         kf = KalmanFilter(init_x=x, init_v=v, acc_variance=acc_var)
         kf.predict(dt=1)   
-        
+
         self.assertEqual(kf.cov.shape, (2,2))
         self.assertEqual(kf.mean.shape, (2,))
+
+    def test_call_predict_increases_uncertainty(self):
+        x = 0.2
+        v = 0.1
+        acc_var = 0.1
+        kf = KalmanFilter(init_x=x, init_v=v, acc_variance=acc_var)
+
+        for i in range(30):
+            old_cov = np.linalg.det(kf.cov)
+            kf.predict(dt=1)   
+            new_cov = np.linalg.det(kf.cov)
+            self.assertGreater(new_cov, old_cov)
+            print(old_cov, new_cov)
+        
+        
